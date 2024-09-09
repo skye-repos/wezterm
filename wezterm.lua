78ce6400df50fed76c89b5fa8441833fca222b55
Skye <skye@Skyes-MacBook-Pro.local>
2024-09-09 16:11:13 +0530

    colors

--- wezterm.lua
+++ wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.window_background_opacity = 1.0
+config.window_background_opacity = 0.95
 config.font = wezterm.font("0xProto")
 config.font_size = 16.0
 config.color_scheme = "thetasigma"
