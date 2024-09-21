1806e060ccc9cc14e1acc1bdbf093de10b96742a
Skye <skye@Skyes-MacBook-Pro.local>
2024-09-21 18:33:19 +0530

    font size indecision

--- wezterm.lua
+++ wezterm.lua
@@ -3,6 +3,6 @@ local config = wezterm.config_builder()
 
 config.window_background_opacity = 1.0
 config.font = wezterm.font("0xProto")
-config.font_size = 16.0
+config.font_size = 22.0
 config.color_scheme = "thetasigma"
 return config
